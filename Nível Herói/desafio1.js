4f3935e3f95ccbd84e9cd75456cf3a2593c6dde7
metalbear1 <marcofrontdev@gmail.com>
2025-08-11 21:07:52 -0300

    Teste de programa\303\247\303\245o - N\303\255vel Her\303\263i - Desafio 1

--- Nível Herói/desafio1.js
+++ Nível Herói/desafio1.js
@@ -1,1 +1,22 @@
-console.log("hello");
+let hero = {
+    name: "Ordek",
+    class:"Druida",
+    xp: 7388,
+    hp: 200,
+    spellSlots: 6,
+    dungeonRNG: parseInt(Math.random() * 10)
+}
+
+let contador = 10;
+
+let monster = {
+  name: "Ozymand, The Lich",
+  xpReward: 2000,
+  defeat: parseInt(Mathrandom() * contador)
+}
+
+if (monster.defeat >= 8) {
+  console.log(`${hero.name} derrotou ${monster.name} e ganhou ${monster.xpReward} pontos totais de experiência!`);
+  hero.xp += monster.xpReward;
+  console.log(`Agora ${hero.name} possui ${hero.xp} pontos totais de experiência! Alcançando assim o nível Imortal!`);
+}
